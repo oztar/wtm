2395cd9dbd1f0682c8775b22dfd777f0af34fbd6
oztar <frediroman@gmail.com>
2022-03-18 15:55:58 +0100

    update

---
+++ wtm_test-0.0.js
@@ -0,0 +1,24 @@
+'use strict'
+let interval;
+
+const load = function(socketID){
+    this.emit(socketID,'Initial Test');
+    interval = setInterval( ()=>{
+	if( socketID != null){
+	    this.emit(socketID,'Module->test');
+	}
+    },1000);
+}
+
+const unload = function(socketID){
+    clearInterval(interval);
+    if( socketID != null){
+	this.emit(socketID,'Unload Module TEST');
+    }
+}
+module.exports = {
+    command : {},
+    load,
+    unload,
+    autoload :true
+}
